cee4de519436a04a83268769a2e75d4f85bc07c0
Ernest TSAMO <ernesttsamo@Ernests-MacBook-Pro.local>
2024-10-07 12:11:32 +0100

    fix: update user info update command

--- app/Console/Commands/UpdateUserInfoCommand.php
+++ app/Console/Commands/UpdateUserInfoCommand.php
@@ -5,27 +5,49 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\User;
 use Faker\Factory as Faker;
+use DateTimeZone;
 
 class UpdateUserInfoCommand extends Command
 {
-    protected $signature = 'users:update';
-    protected $description = 'Update users with random names and timezones';
+    protected $signature = 'users:update {count? : Number of users to update}';
+    protected $description = 'Update users with random names or timezones';
 
     public function handle()
     {
         $faker = Faker::create();
-        $timezones = ['CEST', 'CST', 'GMT+1'];
+        $timezones = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
 
-        User::all()->each(function ($user) use ($faker, $timezones) {
-            $firstName = $faker->firstName;
-            $lastName = $faker->lastName;
+        $totalUsers = User::count();
+        $count = $this->argument('count');
 
-            $user->update([
-                'name' => "$firstName $lastName",
-                'timezone' => $timezones[array_rand($timezones)],
-            ]);
-        });
+        if ($count === null) {
+            $usersToUpdate = User::all();
+            $this->info("Updating all {$totalUsers} users.");
+        } else {
+            $count = min((int)$count, $totalUsers);
+            $usersToUpdate = User::inRandomOrder()->take($count)->get();
+            $this->info("Updating {$count} out of {$totalUsers} users.");
+        }
 
-        $this->info('Users updated successfully!');
+        $updatedCount = 0;
+
+        foreach ($usersToUpdate as $user) {
+            if (rand(0, 1) == 0) {
+                $oldName = $user->name;
+                $newName = $faker->name();
+                $user->name = $newName;
+                $this->info("Updated user {$user->id}: Name changed from '{$oldName}' to '{$newName}'");
+            } else {
+                $oldTimezone = $user->timezone;
+                $newTimezone = $faker->randomElement($timezones);
+                $user->timezone = $newTimezone;
+                $this->info("Updated user {$user->id}: Timezone changed from '{$oldTimezone}' to '{$newTimezone}'");
+            }
+
+            $user->save();
+            $updatedCount++;
+        }
+
+        $this->info("{$updatedCount} users updated successfully!");
     }
 }
